49c929593a53778a643a67bb591d7a5d839f4bb5
mayaoumana <oumana.raissi@gmail.com>
2023-06-01 09:50:59 +0200

    ok

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,21 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    chromeWebSecurity: false
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
+    },
   },
 });
